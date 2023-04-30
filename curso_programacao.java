88e52a19ec92103912723e75ab5f29375e25bcd2
rogtavares <rogerio@almeidatavares.com.br>
2023-04-30 12:05:53 -0300

    Comiint v12 readme e dados
    introducao do exercicio da aula 17 N.A

---
+++ curso_programacao.java
@@ -0,0 +1,10 @@
+public class curso_programacao
+{
+    public static void main(String[] args) {
+        System.out.println();
+
+        //aula17
+        //Até o momento conseguimos criar uma classe e adicionar um método main()
+        // junto com o método de impressão sem muito esforço, mas como podemos fazer para executar o método main()?
+    }
+}
