c110b1084297eabeae94d3072ae4169b4970414e
rogtavares <rogerio@almeidatavares.com.br>
2023-04-30 12:08:45 -0300

    Comiint v3  correcao de dados commini2
     //aula17 feito

--- curso_programacao.java
+++ curso_programacao.java
@@ -3,7 +3,8 @@ public class curso_programacao
     public static void main(String[] args) {
         System.out.println();
 
-        //aula17
+        //aula17 e feito
+
         //Até o momento conseguimos criar uma classe e adicionar um método main()
         // junto com o método de impressão sem muito esforço, mas como podemos fazer para executar o método main()?
     }
